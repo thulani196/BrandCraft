6cca260c71fd85acc89dbf5321ba9fcd350e6803
thulani196 <tembothulani96@outlook.com>
2018-01-10 10:51:22 +0200

    Editing index page

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -8,7 +8,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Laravel') }}</title>
+    <title>BrandCraft</title>
     {{--  Icons  --}}
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
     <!-- Styles -->
